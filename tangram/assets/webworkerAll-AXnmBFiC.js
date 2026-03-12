ea8959b1ac3f6ca1d23ecc3beb5e7b9ec97372a9
DevWagnerEmerich <dev.wagneremerich@gmail.com>
2026-03-12 17:54:32 -0300

    fix(tangram): apply user recognition flow with early postMessage listener

--- tangram/assets/webworkerAll-AXnmBFiC.js
+++ tangram/assets/webworkerAll-AXnmBFiC.js
@@ -1,4 +1,4 @@
-import{G as R,b as S,i as b,M as P,N as I,I as B,a0 as T,T as _,ah as O,R as A,w as C,$ as E,p as k}from"./index-qrLbF6tk.js";import{F as z}from"./Filter-Cq5fHyfP.js";var U=`in vec2 aPosition;
+import{G as R,b as S,i as b,M as P,N as I,I as B,a0 as T,T as _,ah as O,R as A,w as C,$ as E,p as k}from"./index-M46f5-sG.js";import{F as z}from"./Filter-DM2pEBQj.js";var U=`in vec2 aPosition;
 out vec2 vTextureCoord;
 
 uniform vec4 uInputSize;
